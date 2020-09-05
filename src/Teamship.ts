3bd7da19fa58ca36a85f3c56baedd25351d53f40
Kazuichi Takashiro <takashiro@qq.com>
2020-09-05 14:30:58 +0800

    Add Teamship

---
+++ src/Teamship.ts
@@ -0,0 +1,25 @@
+import Role from './Role';
+import Team from './Team';
+
+const ship = new Map<Role, Team>();
+
+ship.set(Role.Unknown, Team.Unknown);
+ship.set(Role.Werewolf, Team.Werewolf);
+ship.set(Role.Villager, Team.Villager);
+ship.set(Role.Seer, Team.Villager);
+ship.set(Role.Tanner, Team.Tanner);
+ship.set(Role.Minion, Team.Werewolf);
+ship.set(Role.Troublemaker, Team.Villager);
+ship.set(Role.Robber, Team.Villager);
+ship.set(Role.Drunk, Team.Villager);
+ship.set(Role.Mason, Team.Villager);
+ship.set(Role.Hunter, Team.Villager);
+ship.set(Role.Witch, Team.Villager);
+ship.set(Role.AlphaWolf, Team.Werewolf);
+ship.set(Role.DreamWolf, Team.Werewolf);
+ship.set(Role.Prince, Team.Villager);
+ship.set(Role.MysticWolf, Team.Werewolf);
+ship.set(Role.Cursed, Team.Villager);
+ship.set(Role.ParanormalInvestigator, Team.Villager);
+
+export default ship;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Team } from './Team';
+export { default as Teamship } from './Teamship';
 export { default as Role } from './Role';
