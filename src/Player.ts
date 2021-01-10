adafc42c9a7a5bd51c3697c9bb4a98a32fbf76ff
Kazuichi Takashiro <takashiro@qq.com>
2021-01-10 15:55:22 +0800

    Add accessories to Player

--- src/Player.ts
+++ src/Player.ts
@@ -1,8 +1,13 @@
+import Artifact from './Artifact';
+import Mark from './Mark';
 import Role from './Role';
 
 interface Player {
 	seat: number;
 	role: Role;
+	shielded?: boolean;
+	artifacts?: Artifact[];
+	marks: Mark[];
 }
 
 export default Player;
